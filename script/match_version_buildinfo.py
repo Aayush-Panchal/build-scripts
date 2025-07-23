776b0e88398417b430ef228de4b4d44e72720d2c
Aayush-Panchal <Aayush.Panchal@ibm.com>
2025-07-23 15:58:22 +0530

    Update match_version_buildinfo.py

--- script/match_version_buildinfo.py
+++ script/match_version_buildinfo.py
@@ -1,27 +1,21 @@
 import json
 import os
 import re
-
 version = str(os.environ['VERSION'])
-match_version = ""
-
-# Load JSON
-with open('build_info.json') as f:
-    data = json.load(f)
-
-# Search for matching version key
-for key, value in data.items():
-    subKeys = [subKey.strip() for subKey in key.split(',')]
+match_version=""
+f = open('build_info.json')
+data = json.load(f)
+for key,value in data.items():
+  subKeys = [subKey.strip() for subKey in key.split(',')]
+  if version in subKeys:
+    match_version = key
+    break
+  else:
     for subKey in subKeys:
-        if subKey == version:
-            match_version = key
-            break
-        # Convert wildcard to regex pattern
-        regex_str = '^' + re.escape(subKey).replace("\\*", ".*") + '$'
-        if re.fullmatch(regex_str, version):
-            match_version = key
-            break
-    if match_version:
-        break
-
+      regex_str = '^' + subKey.replace(".", "\\.").replace("*", ".*") + '$'
+      regex = re.compile(regex_str)
+      if regex.match(version):
+        match_version = key
+  if len(match_version) != 0:
+    break
 print(match_version)
