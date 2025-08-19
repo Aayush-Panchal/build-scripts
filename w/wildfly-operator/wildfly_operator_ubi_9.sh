e9b9675283c21fabf4c2d261292a554c94a1bbfb
Aayush-Panchal <Aayush.Panchal@ibm.com>
2025-08-19 11:58:31 +0530

    Update wildfly_operator_ubi_9.sh

--- w/wildfly-operator/wildfly_operator_ubi_9.sh
+++ w/wildfly-operator/wildfly_operator_ubi_9.sh
@@ -22,8 +22,30 @@ PACKAGE_VERSION=${1:-1.1.3}
 PACKAGE_URL=https://github.com/wildfly/wildfly-operator.git
 export GO_VERSION=${GO_VERSION:-1.21.0}
 
-yum install -y git gcc wget make
+### FIX START: Docker image name sanitization
+# If script is invoked with an image name argument (e.g., "ubi9 wildfly"),
+# ensure it is converted to proper format "ubi9:wildfly"
+if [ -n "$3" ]; then
+  RAW_IMAGE_NAME="$3"
+  if [[ "$RAW_IMAGE_NAME" == *" "* ]]; then
+    IMAGE_PART1=$(echo "$RAW_IMAGE_NAME" | awk '{print $1}')
+    IMAGE_PART2=$(echo "$RAW_IMAGE_NAME" | awk '{print $2}')
+    FIXED_IMAGE_NAME="${IMAGE_PART1}:${IMAGE_PART2}"
+    echo "[FIX] Corrected Docker image name from '$RAW_IMAGE_NAME' to '$FIXED_IMAGE_NAME'"
+    export VALIDATED_IMAGE_NAME="$FIXED_IMAGE_NAME"
+  else
+    export VALIDATED_IMAGE_NAME="$RAW_IMAGE_NAME"
+  fi
+  # Hard check for valid repo:tag format
+  if [[ "$VALIDATED_IMAGE_NAME" != *":"* ]]; then
+    echo "[ERROR] Invalid Docker image reference: $VALIDATED_IMAGE_NAME"
+    echo "Expected format 'repository:tag'"
+    exit 1
+  fi
+fi
+### FIX END
 
+yum install -y git gcc wget make
 
 wget https://golang.org/dl/go$GO_VERSION.linux-ppc64le.tar.gz
 tar -C /usr/local -xvzf go$GO_VERSION.linux-ppc64le.tar.gz
@@ -41,7 +63,6 @@ git clone $PACKAGE_URL $PACKAGE_NAME
 cd $PACKAGE_NAME
 git checkout $PACKAGE_VERSION
 
-
 if ! make build; then
     echo "------------------$PACKAGE_NAME:build_fails---------------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
@@ -56,21 +77,8 @@ if ! make unit-test; then
 else
     echo "------------------$PACKAGE_NAME:unit_test_pass-----------------------------------"
     echo "$PACKAGE_URL $PACKAGE_NAME"
-	echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Install_and_Test_Success"
+    echo "$PACKAGE_NAME  | $PACKAGE_VERSION | $OS_NAME | GitHub  | Pass |  Install_and_Test_Success"
     exit 0
 fi
 
-#e2e tests has dependency on below images which are not available for Power -
-#quay.io/operator-framework/scorecard-test:v1.3.1
-#quay.io/wildfly-quickstarts/wildfly-operator-quickstart:bootable-21.0
-#quay.io/wildfly-quickstarts/clusterbench:latest
-
-#Minikube setup is required for E2E tests execution commenting this as minikube doesnt work within container. 
-# Steps for Minikube Setup -
-#1. curl -LO https://storage.googleapis.com/minikube/releases/latest/minikube-linux-ppc64le
-#2. sudo install minikube-linux-ppc64le /usr/local/bin/minikube
-#3. minikube start --driver=docker - to start mimnikube container
-#4. minikube status - check minikube status running and configured
-
-#For E2E tests run -
-#eval $(minikube -p minikube docker-env) && make test-e2e-minikube
+# e2e tests have dependencies not available on Power, skipped.
